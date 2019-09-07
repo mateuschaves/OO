650d8135e860aa3b90e480856122efefeb28265e
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-09-07 18:19:32 -0300

    dealing exceptions

--- Projeto/Main.java
+++ Projeto/Main.java
@@ -1,19 +1,24 @@
 class Main {
     public static void main(String[] args) {
         Television tv = new Television();
+        try {
+            tv.setStatus(1);
+            tv.setBrightness(50);
+            tv.setChannelSelected(0);
+            tv.setManufacturer("Samsung");
+            tv.setSerialNumber(155877454);
+            tv.setVolume(30);
 
-        tv.setBrightness(50);
-        tv.setChannelSelected(0);
-        tv.setManufacturer("Samsung");
-        tv.setSerialNumber(155877454);
-        tv.setVolume(30);
+            System.out.println("Volume: " + tv.getVolume());
+            System.out.println("Brilho: " + tv.getBrightness());
+            System.out.println("Canal selecionado: " + tv.getChannelSelected());
+            System.out.println("Fabricante: " + tv.getManufacturer());
+            System.out.println("Número de série: " + tv.getSerialNumber());
+            System.out.println("Fabricante: " + tv.getManufacturer());
 
-        System.out.println("Volume: " + tv.getVolume());
-        System.out.println("Brilho: " + tv.getBrightness());
-        System.out.println("Canal selecionado: " + tv.getChannelSelected());
-        System.out.println("Fabricante: " + tv.getManufacturer());
-        System.out.println("Número de série: " + tv.getSerialNumber());
-        System.out.println("Fabricante: " + tv.getManufacturer());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
--- Projeto/Television.java
+++ Projeto/Television.java
@@ -1,47 +1,89 @@
 class Television {
+    /*
+     * 0 - Desligada 1 - Ligada 2 - standby
+     */
+    private int status;
     private int brightness;
     private int volume;
     private int channelSelected;
     private String manufacturer;
     private int serialNumber;
 
-    public int getBrightness() {
+    public int getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getBrightness() throws Exception {
+        if (this.getStatus() == 0) {
+            throw new Exception("Televisão desligada !");
+        }
         return this.brightness;
     }
 
-    public void setBrightness(int brightness) {
+    public void setBrightness(int brightness) throws Exception {
+        if (this.getStatus() == 0) {
+            throw new Exception("Televisão desligada !");
+        }
         this.brightness = brightness;
     }
 
-    public int getVolume() {
+    public int getVolume() throws Exception {
+        if (this.getStatus() == 0) {
+            throw new Exception("Televisão desligada !");
+        }
         return this.volume;
     }
 
-    public void setVolume(int volume) {
+    public void setVolume(int volume) throws Exception {
+        if (this.getStatus() == 0) {
+            throw new Exception("Televisão desligada !");
+        }
         this.volume = volume;
     }
 
-    public int getChannelSelected() {
+    public int getChannelSelected() throws Exception {
+        if (this.getStatus() == 0) {
+            throw new Exception("Televisão desligada !");
+        }
         return this.channelSelected;
     }
 
-    public void setChannelSelected(int channelSelected) {
+    public void setChannelSelected(int channelSelected) throws Exception {
+        if (this.getStatus() == 0) {
+            throw new Exception("Televisão desligada !");
+        }
         this.channelSelected = channelSelected;
     }
 
-    public String getManufacturer() {
+    public String getManufacturer() throws Exception {
+        if (this.getStatus() == 0) {
+            throw new Exception("Televisão desligada !");
+        }
         return this.manufacturer;
     }
 
-    public void setManufacturer(String manufacturer) {
+    public void setManufacturer(String manufacturer) throws Exception {
+        if (this.getStatus() == 0) {
+            throw new Exception("Televisão desligada !");
+        }
         this.manufacturer = manufacturer;
     }
 
-    public int getSerialNumber() {
+    public int getSerialNumber() throws Exception {
+        if (this.getStatus() == 0) {
+            throw new Exception("Televisão desligada !");
+        }
         return this.serialNumber;
     }
 
-    public void setSerialNumber(int serialNumber) {
+    public void setSerialNumber(int serialNumber) throws Exception {
+        if (this.getStatus() == 0) {
+            throw new Exception("Televisão desligada !");
+        }
         this.serialNumber = serialNumber;
     }
 
