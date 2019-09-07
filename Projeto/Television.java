a76c950c9ee688da396481c44167714982cb888d
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-09-07 17:55:09 -0300

    Getters and Setter Television Class

--- Projeto/Television.java
+++ Projeto/Television.java
@@ -4,4 +4,45 @@ class Television {
     int channelSelected;
     String manufacturer;
     int serialNumber;
+
+    public int getBrightness() {
+        return this.brightness;
+    }
+
+    public void setBrightness(int brightness) {
+        this.brightness = brightness;
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getChannelSelected() {
+        return this.channelSelected;
+    }
+
+    public void setChannelSelected(int channelSelected) {
+        this.channelSelected = channelSelected;
+    }
+
+    public String getManufacturer() {
+        return this.manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getSerialNumber() {
+        return this.serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
 }
